d6470f7b5e93e89bd1d1b4dc9d6f0e67dd744b13
Matt Miles <matt_miles2@live.co.uk>
2025-03-24 19:15:34 +0000

    Add route_root view and update api settings.

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -35,7 +35,15 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DATETIME_FORMAT': '%d %b %Y',
 }
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
